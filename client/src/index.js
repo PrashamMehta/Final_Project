8a8c9398e875af7d372f64899bceca10c5ae643e
PrashamMehta <141221535+PrashamMehta@users.noreply.github.com>
2024-04-30 16:22:23 +0530

    !2th Commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
+import { SearchContextProviderTrain } from "./context/SearchContextTrain";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-      </SearchContextProvider>
+      <SearchContextProviderTrain>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </SearchContextProviderTrain>
     </AuthContextProvider>
   </React.StrictMode>
 );
